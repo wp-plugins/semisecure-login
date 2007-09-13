e5765687dfd0f109f34db4ba15a7daaabd1ab787
slthytove <slthytove@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2007-09-13 11:21:28 +0000

    git-svn-id: https://plugins.svn.wordpress.org/semisecure-login/trunk@18691 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ readme.txt
@@ -0,0 +1,30 @@
+=== Semisecure Login ===
+Contributors: slthytove
+Donate link: https://www.paypal.com/cgi-bin/webscr?cmd=_xclick&business=james%2em%2eallen%40gmail%2ecom&item_name=Semisecure%20Login%20Donation%20%2d%20Please%20feel%20free%20to%20donate%20any%20amount%2e%20Your%20contributions%20are%20appreciated%21&currency_code=USD&bn=PP%2dDonationsBF
+Tags: admin, login, md5, hash, encryption, security, password
+Requires at least: 2.1
+Tested up to: 2.2
+Stable tag: 1.0
+
+Semisecure Login increases the security of the login process using client-side MD5 encryption on the password when a user logs in.
+
+== Description ==
+
+Semisecure Login increases the security of the login process using client-side MD5 encryption on the password when a user logs in. JavaScript is required to enable encryption. It is most useful for situations where SSL is not available, but the administrator wishes to have some additional security measures in place without sacrificing convenience.
+
+== Installation ==
+
+1. Upload the entire `semisecure-login/` directory to the `/wp-content/plugins/` directory
+1. Activate the plugin through the 'Plugins' menu in WordPress
+
+== Frequently Asked Questions ==
+
+= How does this work? =
+
+A user attempts to log in via the login page. If JavaScript is enabled, the password along with a nonce is MD5-hashed, and the original (unencrypted) password is not sent. The server compares the received version with the expected version.
+
+If JavaScript is not enabled, the password is sent in cleartext just like normal. This is inherently insecure over plaintext channels, but it is the default behavior of WordPress.
+
+= Is this really secure? =
+
+It is not as secure as SSL, but it should prevent a large number of potential hijackings, as long as users are using modern browsers with JavaScript enabled. I obviously make no guarantees.
---
+++ semisecure-login.php
@@ -0,0 +1,121 @@
+<?php
+/*
+Plugin Name: Semisecure Login
+Plugin URI: http://jamesmallen.net/
+Description: Semisecure Login increases the security of the login process using client-side MD5 encryption on the password when a user logs in. JavaScript is required to enable encryption.
+Version: 1.0
+Author: James M. Allen
+Author URI: http://jamesmallen.net/
+*/
+
+/*  Copyright 2007 James M. Allen (email : james.m.allen@gmail.com)
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+add_action('login_head', array('SemisecureLogin', 'login_head'));
+add_action('login_form', array('SemisecureLogin', 'login_form'));
+add_action('wp_authenticate', array('SemisecureLogin', 'authenticate'), 1, 2);
+
+
+if (! class_exists('SemisecureLogin')) {
+	class SemisecureLogin {
+		/*
+		 * Plugin hooks
+		 */
+		
+		/*
+		 * Sets the nonce, includes JavaScript for preparing the login form
+		 */
+		function login_head() {
+			session_start();
+			
+			// always generate a new nonce
+			$_SESSION['login_nonce'] = md5(rand());
+			
+			
+			?>
+		<script type="text/javascript" src="<?php echo get_option('siteurl');?>/wp-content/plugins/semisecure-login/md5.js"></script>
+		<script type="text/javascript">
+		function hashPwd() {
+			var formLogin = document.getElementById('loginform');
+			
+			var userLog = document.getElementById('user_login');
+			var userPwd = document.getElementById('user_pass');
+			
+			var password = userPwd.value;
+			
+			var userMD5Pwd = document.createElement('input');
+			userMD5Pwd.setAttribute('type', 'hidden');
+			userMD5Pwd.setAttribute('id', 'user_pass_md5');
+			userMD5Pwd.setAttribute('name', 'pwd_md5');
+			userMD5Pwd.value = hex_md5(hex_md5(password) + '<?php echo $_SESSION['login_nonce']?>');
+			formLogin.appendChild(userMD5Pwd);
+			
+			userPwd.value = '';
+			for (var i = 0; i < password.length; i++) {
+				userPwd.value += '*';
+			}
+			
+			return true;
+		}
+		</script>
+		<?php
+		}
+		
+		/*
+		 * Applies event handlers to the form (DOM needs to be ready before this happens)
+		 */
+		function login_form() {
+			?>
+		<script language="javascript" type="text/javascript">
+			var formLogin = document.getElementById('loginform');
+			formLogin.setAttribute('onsubmit', 'return hashPwd();');
+		</script>
+		<?php
+		}
+		
+		
+		/*
+		 * Authenticates users if hashed password is sent.
+		 * Does nothing if no hashed password was sent.
+		 */
+		function authenticate($username, $password) {
+			global $using_cookie;
+			
+			// only do anything if pwd_md5 is set - otherwise degrade gracefully
+			if (!empty($_POST['pwd_md5'])) {
+				session_start();
+				
+				$user = get_userdatabylogin($username);
+				
+				if ( ($user && $username == $user->user_login) &&
+				     (md5($user->user_pass . $_SESSION['login_nonce']) == $_POST['pwd_md5']) ) {
+					$password = md5($user->user_pass);
+					$using_cookie = true;
+					wp_setcookie($user->user_login, $password, $using_cookie);
+				}
+			}
+			
+			// degrades in the absence of JavaScript
+		}
+	}
+}
+
+
+
+
+
+?>
